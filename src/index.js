28777685f26e597e1a6f9a3c1e2dbf927f2d0ae8
ViNezh <126874232+ViNezh@users.noreply.github.com>
2023-08-18 19:40:24 +0300

    task ended

--- src/index.js
+++ src/index.js
@@ -4,23 +4,66 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 //
 const formEl = document.querySelector('.search-form');
-const gallery=document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+const PHOTOS_PER_PAGE = 40;
+let totalPages = 0;
+let currentPage = 1;
+let galleryPhoto = {};
+
+loadMore.addEventListener('click', handlerLoadMore);
 formEl.addEventListener('submit', onFormSubmit);
+
 function onFormSubmit(evt) {
+  loadMore.classList.add('is-hidden');
   evt.preventDefault();
-gallery.innerHTML='';
+  gallery.innerHTML = '';
   const searchReq = formEl.elements.searchQuery.value;
-  getPhoto(searchReq)
-    .then((data) => {
-      gallery.insertAdjacentHTML('beforeend',makeMarkup(data.hits))
-      let galleryPhoto = new SimpleLightbox('.gallery_link',
-    {    navText:    ['&lsaquo;','&rsaquo;'],    }
-  );
+  currentPage = 1;
+  getPhoto(searchReq, currentPage)
+    .then(data => {
+      if (data.hits.length === 0) {
+        Notiflix.Notify.warning(
+          'Sorry, there are no images matching your search query. Please try again.',
+          {
+            width: '560px',
+            position: 'center-center',
+            fontSize: '32px',
+            timeout: 6000,
+          }
+        );
+        return;
+      }
+      totalPages = Math.ceil(data.totalHits / data.hits.length);
+      console.log(totalPages);
+      gallery.insertAdjacentHTML('beforeend', makeMarkup(data.hits));
+      galleryPhoto = new SimpleLightbox('.gallery_link', {
+        navText: ['&lsaquo;', '&rsaquo;'],
+      });
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      loadMore.classList.remove('is-hidden');
+      scrolling();
+      endOfPages();
     })
     .catch(error => console.log(error));
 }
 
-async function getPhoto(request) {
+function handlerLoadMore() {
+  loadMore.classList.add('is-hidden');
+  currentPage += 1;
+  const searchReq = formEl.elements.searchQuery.value;
+  getPhoto(searchReq, currentPage)
+    .then(data => {
+      gallery.insertAdjacentHTML('beforeend', makeMarkup(data.hits));
+      galleryPhoto.refresh();
+      loadMore.classList.remove('is-hidden');
+      scrolling();
+      endOfPages();
+    })
+    .catch(error => console.log(error));
+}
+
+async function getPhoto(request, currentPage) {
   const url = 'https://pixabay.com/api/?';
   const searchParams = new URLSearchParams({
     key: '38856418-a7b3dde49805ba60b9b57505c',
@@ -28,8 +71,8 @@ async function getPhoto(request) {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    page: 1,
-    per_page: 40,
+    page: currentPage,
+    per_page: PHOTOS_PER_PAGE,
   });
   try {
     const getData = await axios.get(`${url}${searchParams}`);
@@ -38,13 +81,22 @@ async function getPhoto(request) {
     console.log(error.message);
   }
 }
-function makeMarkup(arr){
-return arr.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads})=>
-    `<div class="photo-card">
-    <a href="${largeImageURL}" class="gallery_link">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300px" height="200px"/>
-      </a>
-      <div class="info">
+function makeMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<a href="${largeImageURL}" class="gallery_link">
+        <div class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300px" height="200px"/>
+           <div class="info">
         <p class="info-item">
           <b>Likes:${likes}</b>
         </p>
@@ -59,6 +111,31 @@ return arr.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads}
         </p>
       </div>
     </div>
+     </a>
   `
-).join(' ')
-};
+    )
+    .join(' ');
+}
+function endOfPages() {
+  if (totalPages === currentPage) {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results.",
+      {
+        width: '640px',
+        position: 'center-top',
+        fontSize: '18px',
+        timeout: 3000,
+      }
+    );
+    loadMore.classList.add('is-hidden');
+  }
+}
+function scrolling() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
